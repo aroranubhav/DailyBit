c319c0911ba58d1cec6cce2238edff63cd0b6433
anubhav <arora.anubhav93@gmail.com>
2021-01-05 23:26:43 +0530

    a better solution to the last word length problem

--- WordLength.py
+++ WordLength.py
@@ -3,11 +3,16 @@
    Eg: "The is a string"
    return 5, as string is 5 characters long
 """
+
 class Solution:
     def __init__(self, string: str):
         self.string = string
 
-    def word_length(self) -> int:
+    """
+       Complexity : Time : O(n) | Space : O(n)
+    """
+
+    def word_length_1(self) -> int:
         length = 0
         for char in reversed(self.string.strip()):
             if char == ' ':
@@ -15,8 +20,33 @@ class Solution:
             else:
                 length += 1
 
+    """
+       Complexity : Time : O(n) | Space : O(1)
+    """
+
+    def word_length(self) -> int:
+        length = 0
+        trailingspace = 0
+
+        for char in reversed(self.string):
+            if char == ' ':
+                trailingspace += 1
+            else:
+                break
+
+        start_idx = len(self.string) - trailingspace - 1
+
+        for idx in range(start_idx, 0, -1):
+            if self.string[idx] == ' ':
+                return length
+            else:
+                length += 1
+
+        return len(self.string) - trailingspace
+
 wordlen = Solution(input("Enter the string: "))
 print(wordlen.word_length())
+
 
 
 
