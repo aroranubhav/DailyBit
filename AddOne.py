800be6e143fea37d83ffa8c8a2615676929dd9d0
anubhav <arora.anubhav93@gmail.com>
2021-03-02 19:48:41 +0530

    solution for add one

---
+++ AddOne.py
@@ -0,0 +1,39 @@
+"""
+    Given a non-empty array of decimal digits representing a non-negative integer, increment one to the integer.
+
+    The digits are stored such that the most significant digit is at the head of the list,
+    and each element in the array contains a single digit.
+
+    You may assume the integer does not contain any leading zero, except the number 0 itself.
+
+    Example 1:
+    Input: digits = [1,2,3]
+    Output: [1,2,4]
+    Explanation: The array represents the integer 123.
+
+    Example 2:
+    Input: digits = [4,3,2,1]
+    Output: [4,3,2,2]
+    Explanation: The array represents the integer 4321.
+
+    Example 3:
+    Input: digits = [0]
+    Output: [1]
+"""
+class Solution:
+    def add_one(self, digits: list[int]) -> list[int]:
+        for i in range(len(digits) - 1, -1, -1):
+            if i == 0 and digits[i] == 9:
+                digits[i] = 0
+                digits.insert(0, 1)
+            elif digits[i] == 9:
+                digits[i] = 0
+            else:
+                digits[i] += 1
+                break
+
+        return digits
+
+"""
+    Complexity : Time : O(n) | Space : O(1)
+"""
